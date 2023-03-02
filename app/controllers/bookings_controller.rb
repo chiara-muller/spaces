9189708aea53e1a74c7b203e4ded877998b50035
Chiara Muller <chiaramuller@hotmail.be>
2023-03-02 16:48:31 +1100

    booking-tests

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,16 +10,16 @@ class BookingsController < ApplicationController
 
   def new
     @user_id = 1
-    @booking = Booking.new
     @space = Space.find(params[:space_id])
+    @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
     @space = Space.find(params[:space_id])
+    @booking.space = @space
     @user_id = 1
     @booking.user_id = @user_id
-    @booking.space = @space
     if @booking.save
       flash[:success]
       redirect_to bookings_path(@booking)
