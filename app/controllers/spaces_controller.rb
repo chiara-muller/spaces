4bfd153b540704d4fdac71b2ec281c2657b4d5d4
Reibang Chakma c.reibang@gmail.com <c.reibang@gmail.com>
2023-03-02 14:33:10 +1100

    Can add multiple photos

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -41,6 +41,6 @@ class SpacesController < ApplicationController
   private
 
   def space_params
-    params.require(:space).permit(:title, :address, :capacity, :space_type, :price, :user_id, :photo)
+    params.require(:space).permit(:title, :address, :capacity, :space_type, :price, :user_id, photos: [])
   end
 end
